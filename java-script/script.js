2839cffa3d89713b42b8a1ee161081a4b95ede10
muhammedjasim <jasimwayanad961@gmail.com>
2023-12-06 21:41:33 -1200

    js trail

---
+++ java-script/script.js
@@ -0,0 +1,61 @@
+//alert('Hello world');
+//onsole.log('hello');
+
+// function findSum(num1, num2) {
+//     return  num1 + num2;
+  
+// }
+// let sum = findSum(4, 5);
+// alert(sum);
+// console.log(sum);
+// let result = sum + 6;
+// alert(result);
+// console.log(result);
+
+// let age = 20;
+
+// if (age >= 18) {
+//     alert("Your Application Submitted");
+// } else {
+//     alert("You are not eligible at this age");
+// }
+
+// let color =  "Yellow";
+
+
+
+//DOM-DOCUMENT OBJECT MODEL
+//const color = element.value;
+
+//function FindFavoriteFruits() {
+    //const element = document.getElementById("color")
+     
+    //console.log(element.style.backgroundColor = 'red');
+   // element.style.backgroundColor = 'blue'
+
+// if (color === "orange") {
+//     console.log("You like to Eat Orange");
+// } else if (color === "yellow") {
+//     console.log("You like to Eat Banana");
+// } else {
+//     console.log("You don't Like Fruits");
+// }
+//}
+
+// let student = {
+//     firstName: "Muhammad",
+//     lastName: "jasim",
+//     age: 22,
+//     isHePassed: true,
+// };
+// let car = {
+//     model: "KIA", number: "kl127094", drive: function() {
+//         console.log("Reaady");
+//         return "ok ready";
+//     }
+// }
+// console.log(car.drive());
+
+
+const fruits = ["banana", "Apple", 2.2, true];
+console.log(fruits[fruits.length - 2]);
