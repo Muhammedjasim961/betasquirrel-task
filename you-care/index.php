a9c42c684fdfedba727cce2ac362d4c0d7717724
muhammedjasim <jasimwayanad961@gmail.com>
2024-01-22 13:43:39 -1200

    The Charity Project

---
+++ you-care/index.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+    <link href="assets/fontawesome/css/fontawesome.css" rel="stylesheet">
+    <link href="assets/fontawesome/css/brands.css" rel="stylesheet">
+    <link href="assets/fontawesome/css/solid.css" rel="stylesheet">
+    <link rel="stylesheet" href="./style.css">
+    <title>You-Care</title>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col col-sm-12">
+                <nav class="navbar top-fixed" style="background-color: #04AA6D;">
+                    <div class="container">
+                        <a class="navbar-brands" href="#">
+                            <img src="img/logo2.webp" width="65" height="50">
+                            <h2 title="You-Care">YOU CARE</h2>
+                        </a>
+
+                        <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav-bar" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button> -->
+
+
+                        <nav class="navbar navbar-expand-lg" id="nav-bar">
+                            <a class="navbar-brand" aria-current="page" href="#" title="Donate">DONATE</a>
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav-bar">
+                                <span class="navbar-toggler-icon" id="navbarNavAltMarkup"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" id="nav-bar">
+                                <div class="navbar-nav">
+                                    <a class="nav-link" aria-current="page" href="#" title="Home">Home</a>
+                                    <a class="nav-link" href="#" title="About Us">About Us</a>
+                                    <a class="nav-link" href="#" title="contact">Contact</a>
+                                </div>
+                            </div>
+                        </nav>
+
+
+
+                    </div>
+                </nav>
+                <div class="row mt-3">
+                    <div class="col">
+                        <div class="container-image">
+                            <img src="img/papper2.jpg" alt="logo" width="100%" height="120">
+                            <span>Welcome To <h2 title="You_Care">You Care</h2>Charity Organization</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <main>
+            <div class="row mt-3">
+                <div class="col">
+                    <div class="main-container">
+                        <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-indicators">
+                                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                            </div>
+                            <div class="carousel-inner">
+
+                                <div class="carousel-item active" data-bs-interval="1600">
+                                    <img src="img/youcare.webp" class="d-block w-100" width="auto" height="350px" alt="logo">
+                                </div>
+                                <div class="carousel-item" data-bs-interval="2000">
+                                    <img src="img/charity2.jpg" class="d-block w-100" width="auto" height="350px" alt="logo">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="img/charity.jpg" class="d-block w-100" width="auto" height="350px" alt="logo">
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col mt-4">
+                    <div class="about-image">
+                        <img src="img/papper3.avif" class="img-fluid" alt="servies">
+                        <h2>We Are Providing Services Such As: </h2>
+                        <div class="abstract">
+                            <p>
+                                <li><b>Flexible Donation Models:</b> Charities adopting flexible and innovative donation models, such as cryptocurrency donations, micro-donations, and donor-advised funds. These models allow for diverse and accessible ways for people to contribute.</li>
+
+                                <li><b>Public-Private Partnerships:</b> Collaborating with businesses, government agencies, and other non-profit organizations to maximize impact. These partnerships can bring together different resources and expertise to address complex social issues effectively.</li>
+                                <li><b>Community Engagement:</b> Encouraging active involvement and participation from the communities they serve. This could involve community-driven initiatives, feedback mechanisms, and programs developed in collaboration with local populations to ensure relevance and sustainability.</li>
+                                <li><b>Crisis Response and Preparedness:</b> Being agile in responding to crises and emergencies, with well-prepared disaster response plans. Charities might focus on quick mobilization of resources and effective coordination during natural disasters, health crises, or other emergencies.</li>
+                                <li><b>Innovative Fundraising Models:</b> New and innovative fundraising models may emerge. Crowdfunding, impact investing, and other alternative fundraising methods could gain popularity as charities seek diverse sources of funding.</li>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col ">
+                    <div class="food">
+                        <h2>Food And Accommodation</h2>
+                        <div class="row mt-3">
+                            <div class="col-sm-6 mb-3 mb-sm-0 col-md-6">
+                                <div class="card">
+                                    <img src="https://images.prismic.io/oxylabs-sm/a8eb914f-a41a-4f67-be2b-cd66f31302c4_Food+Delivery+Hero+Image%402x.png?auto=compress,format" width="300px" alt="">
+                                    <div class="card-body">
+                                        <h5 class="card-title"><b>
+                                                <h4>Food Distribution</h4>
+                                            </b></h5>
+                                        <p class="card-text">
+                                            <li><b>Food Distribution Programs:</b> Many charity organizations operate food banks or distribution centers where they collect, store, and distribute food to individuals and families in need. This can include non-perishable items, fresh produce, and other essential food items.</li>
+
+                                            <li><b>Meal Programs:</b> Charity organizations may run meal programs, such as soup kitchens or community dining initiatives, where hot meals are prepared and served to those facing hunger. These programs often target homeless individuals, low-income families.</li>
+
+                                            <li><b>Food Pantries:</b> Establishing community-based food pantries that allow individuals and families to access essential food items regularly. providing a variety of goods to help supplement household needs.</li>
+
+                                            <li><b>Emergency Food Relief:</b> Responding to crises or natural disasters by providing immediate emergency food relief to affected communities. This can involve distributing ready-to-eat meals, food kits, and other supplies to those impacted by emergencies.</li>
+
+                                            <li><b>Nutritional Education:</b> Integrating educational components to promote healthy eating habits, nutritional awareness, and cooking skills. Some charities offer workshops or resources to empower recipients to make informed and sustainable food choices.</li>
+                                        </p>
+                                        <a href="#" class="btn btn-primary" title="Find More">Find More</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 mb-3 mb-sm-0 col-md-6">
+                                <div class="card">
+                                    <img src="https://upload.wikimedia.org/wikipedia/commons/4/47/Home_icon_green.png" width="300px" alt="">
+                                    <div class="card-body">
+                                        <h5 class="card-title"><b>
+                                                <h4>Shelters</h4>
+                                            </b> </h5>
+                                        <p class="card-text">
+                                            <li><b>Emergency Shelters:</b> Operating temporary shelters that provide immediate housing for individuals facing homelessness due to various reasons, including natural disasters, economic challenges, or personal crises.</li>
+                                            <li><b>Transitional Housing Programs:</b> Offering transitional housing solutions for individuals and families who need longer-term support to transition from homelessness to stable, independent living. These programs often include supportive services such as counseling, job training, and life skills development.</li>
+                                            <li><b>Homeless Outreach Services:</b> Conducting outreach programs to identify and assist individuals living on the streets or in precarious conditions. Outreach teams may provide basic necessities, information about available shelters, and connections to essential resources.</li>
+                                            <li><b>Permanent Supportive Housing:</b> Collaborating with housing authorities and social service agencies to provide permanent housing solutions for individuals experiencing chronic homelessness, often coupled with ongoing support services.</li>
+                                            <li><b>Family Shelters:</b> Establishing shelters specifically designed to accommodate families, offering safe and secure living spaces for parents and children experiencing homelessness.navigate the housing market, understand tenant rights, and advocate for affordable and stable housing options.</li>
+                                        </p>
+                                        <a href="#" class="btn btn-primary" title="Find More">Find More</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col col-6 mt-4 mx-auto gap-2">
+                                    <div class="meditation-section shadow-sm bg-body-tertiary rounded">
+                                        <img src="https://www.gratitudelodge.com/wp-content/uploads/2022/12/Blog-Image-04-3.png" class="rounded float-start" width="100%" alt="cloth">
+                                        <h2>Providing meditation Camps</h2>
+                                        <div class="col mt-3 float-end mx-auto gap-2">
+                                            <div class="cloth-section shadow-sm bg-body-tertiary rounded">
+                                                <img src="https://images.pexels.com/photos/102129/pexels-photo-102129.jpeg?cs=srgb&dl=pexels-daian-gan-102129.jpg&fm=jpg" class="rounded float-end" width="100%" alt="meditate">
+                                                <h2>Providing Clothing Assistance</h2>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row mt-4">
+                                <div class="col mt-4">
+                                    <div class="about-us">
+                                        <div class="top-section">
+                                            <img src="img/about us.jpg" alt="Avatar" class="image">
+                                            <div class="overlay">
+                                                <div class="text">What Is YOUCARE...?</div>
+                                                <div class="font">
+                                                    <p>
+                                                        <b><u>YouCare</u></b>, a compassionate charity organization founded in 2024, is dedicated to making a meaningful impact on the lives of those in need. With a focus on community well-being, education, and healthcare, YouCare strives to create a brighter future for individuals facing challenges. Through strategic initiatives, community engagement, and a commitment to fostering empathy, YouCare aims to build a world where every person is supported, empowered, and cared for. Together, let's transform lives and inspire a culture of compassion in our global community.
+                                                    </p>
+                                                    <div class="paragraph">
+                                                        <p>
+                                                            <li><b>Humanitarian Aid:</b> Providing emergency relief, such as food, shelter, and medical assistance, to communities affected by natural disasters, conflicts, or crises.</li>
+
+                                                            <li><b>Healthcare Services:</b> Offering medical care, vaccinations, and health education to unreserved communities, with a focus on preventive measures and improving overall well-being.</li>
+
+                                                            <li><b>Education Programs:</b> Establishing schools, scholarship programs, and educational initiatives to ensure access to quality education, especially for marginalized or economically disadvantaged individuals.</li>
+
+                                                            <li><b>Community Development:</b> Implementing projects that enhance infrastructure, access to clean water, sanitation facilities, and other community-driven initiatives to improve overall living conditions.</li>
+
+                                                            <li><b>Social Welfare Programs:</b> Supporting vulnerable populations, such as orphans, the elderly, and people with disabilities, through services like counseling, housing support.</li>
+
+                                                            <li><b> Environmental Conservation:</b> Engaging in initiatives that promote sustainability, environmental awareness, and conservation efforts to address climate change and protect natural resources.</li>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row mb-3">
+                                            <div class="col mb-4">
+                                                <div class="magic">
+                                                    <div class="end-credit">
+                                                        <i class="bi bi-arrow-down-circle-fill"></i>
+                                                        <h3><b>Hover Over Here To See The Magic.</b></h3>
+                                                        <div class="bi">
+                                                            <i class="bi bi-arrow-down-circle-fill"></i>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+    </div>
+    </div>
+    </main>
+    <div class="contain-fluid">
+        <div class="row">
+            <div class="col">
+                <div class="description">
+                    <h1>2024 YOUCARE - All Right Reserved</h1>
+                    <div class="circle">
+                        <i class="fa-solid fa-copyright"></i>
+                    </div>
+                    <div class="media-list">
+                        <div class="instagram">
+                            <i class="fa-brands fa-instagram fa-sm fa-2xl"></i>
+                        </div>
+                        <div class="whatsup">
+                            <i class="fa-brands fa-whatsapp fa-2xl"></i>
+                        </div>
+                        <div class="facebook">
+                            <i class="fa-brands fa-facebook fa-2xl"></i>
+                        </div>
+                        <div class="twitter">
+                            <i class="fa-brands fa-twitter fa-2xl"></i>
+                        </div>
+                    </div>
+                    <div class="policies">
+                        <h1>Privacy policies</h1>
+                        <h2>Legal</h2>
+                        <b>Contact Us</b>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
